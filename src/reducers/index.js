0cebc98409d738f35c4851ae27a3606d22717516
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-22 20:06:53 -0400

    Done example

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -15,7 +15,20 @@ const mockTasks = [ {
 export default function tasks(state = { tasks: mockTasks }, action) {
     switch (action.type) {
         case 'CREATE_TASK': {
-            return [...state, action.payload]
+            console.log(state.tasks);
+            return { tasks: [...state.tasks, action.payload]}
+        }
+        case 'EDIT_TASK': {
+            const tasks = state.tasks.map(t => {
+                if(t.id === action.payload.id) {
+                    return Object.assign({}, t, action.payload.params);
+                }
+
+                return t;
+            });
+
+
+            return { tasks };
         }
     }
     return state
