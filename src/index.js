f9c847b22b887fa8e212004c3783ca6539d8ac02
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-23 08:21:53 -0400

    Added react-devtools-extension

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './App';
 import { createStore } from 'redux';
 import tasks from './reducers';
 import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(tasks);
+const store = createStore(tasks, devToolsEnhancer());
 
 ReactDOM.render(
     <Provider store={ store }>
