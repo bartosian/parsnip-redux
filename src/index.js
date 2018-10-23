26a3cc28489075033dde90b78f8c62c538078db9
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-23 09:00:03 -0400

    Refactoring perfomed

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import tasks from './reducers';
 import { Provider } from 'react-redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import  thunk  from 'redux-thunk';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(tasks, devToolsEnhancer());
+const store = createStore(tasks, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
     <Provider store={ store }>
