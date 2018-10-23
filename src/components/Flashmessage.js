6540c9158ba5a799cb97ae60d690532010a028d4
Kiryl Bartashevich <bartosian1989@gmail.com>
2018-10-23 11:48:16 -0400

    Created FlashMessage comp

---
+++ src/components/Flashmessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function FlashMessage(props) {
+    return (
+    <div className="flash­error"> {props.message}
+    </div> );
+}
+
+FlashMessage.defaultProps = {
+    message: 'An error occurred',
+};
